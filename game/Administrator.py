7fdbd101626f20cff524a351074002d99a205482
William <williamstogin2018@u.northwestern.edu>
2018-04-15 18:31:34 -0500

    Created classes and initial constructors for Administrator and DragonCard

--- game/Administrator.py
+++ game/Administrator.py
@@ -1,3 +1,15 @@
 
 
 #{ board: Board, players: Player[?], dragon: DragonCard, deck: DrawPile }
+
+
+class Administrator:
+
+    """ Default constructor
+    Sets all fields to None
+    """
+    def __init__(self):
+        self.board = None
+        self.players = None
+        self.dragon = None
+        self.deck = None
--- game/DragonCard.py
+++ game/DragonCard.py
@@ -1,1 +1,11 @@
 # { isHeld: bool, holdingPlayer: Player }
+
+class DragonCard:
+
+    """ Default constructor
+    Sets isHeld to False and holdingPlayer to none
+    """
+    def __init__(self):
+       self.isHeld = False
+       self.holdingPlayer = None
+
