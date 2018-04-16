f3e63655a1c73906751ed070afe1e15e2499b2ad
William <williamstogin2018@u.northwestern.edu>
2018-04-15 21:58:59 -0500

    Added signatures and docstrings for this homework's requirements

--- game/Administrator.py
+++ game/Administrator.py
@@ -13,3 +13,27 @@ class Administrator:
         self.players = None
         self.dragon = None
         self.deck = None
+
+    """ Determines whether it is legal for a given player to place a given tile on a given board.
+
+        There are two ways a tile placement can be illegal:
+
+        1. The placement of the tile is an elimination move for the player
+            (unless all of the possible moves are elimination moves).
+        2. The tile is not (a possibly rotated version of) one of the tiles of the player.
+
+        Note that the board is passed in via self because the Administrator
+        maintains the board.
+    """
+    def isLegalPlay(self, tile, player):
+        pass
+
+
+    """ Computes the state of the game after the completion of a turn given the
+         state of the game before the turn.
+
+        Note that the draw pile, list of players (in game and eliminated) are
+        passed in self because the administrator keeps track of these things.
+    """
+    def playATurn(self, player,):
+        pass
