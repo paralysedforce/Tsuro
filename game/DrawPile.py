2414d5380908336df4031f433626c385e94f4964
William <williamstogin2018@u.northwestern.edu>
2018-04-15 22:13:48 -0500

    Added deck implementation for instantiation

--- game/DrawPile.py
+++
@@ -1,5 +0,0 @@
-# { cards: MapCard[16] }
-
-class DrawPile:
-    def __init__(self):
-        self.cards = None
---
+++ game/deck.py
@@ -0,0 +1,13 @@
+
+
+class Deck:
+
+
+    """ Creates a deck from the list of known paths"""
+    def __init__(self):
+        pass
+
+
+    possiblePaths = 4
+
+
