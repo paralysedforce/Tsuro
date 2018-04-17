6a56249c5c7e2d42f0d2df7d27f86411ac51ad24
William Stogin <William@William-PC.(none)>
2018-04-16 23:48:30 -0500

    Added setting adjacent squares in MapSquare and tests

--- game/map_square.py
+++ game/map_square.py
@@ -1,6 +1,14 @@
 from .token_spot import TokenSpot
+from enum import Enum
+from sys import stderr
 
 # { spots: TokenSpot[8] }
+
+class Side(Enum):
+    TOP=0
+    RIGHT=1
+    BOTTOM=2
+    LEFT=3
 
 class MapSquare:
 
@@ -32,6 +40,39 @@ class MapSquare:
 
             start.pair_via_path(end)
             end.pair_via_path(start)
+
+    """ Binds the TokenSpots on the borders of adjacent MapSquares
+        
+        Args:
+            other -- MapSquare that is adjacent to self
+            side -- Side showing on which side of self other lies
+    """
+    def set_adjacent(self, other, side):
+        # Sadly, switch doesn't exist :(
+        
+        if side == Side.TOP:
+            # Set the 0th and 1st nodes of self to 5th and 4th nodes of other, respectively
+            TokenSpot.pair_adjacent(self._spots[0], other._spots[5])
+            TokenSpot.pair_adjacent(self._spots[1], other._spots[4])
+        elif side == Side.RIGHT:
+            # Set the 2nd and 3rd nodes of self to 8th and 7th nodes of other, respectively
+            TokenSpot.pair_adjacent(self._spots[2], other._spots[7])
+            TokenSpot.pair_adjacent(self._spots[3], other._spots[6])
+        elif side == Side.BOTTOM:
+            # Set the 4th and 5th nodes of self to 1st and 0th nodes of other, respectively
+            TokenSpot.pair_adjacent(self._spots[4], other._spots[1])
+            TokenSpot.pair_adjacent(self._spots[5], other._spots[0])
+            pass
+        elif side == Side.LEFT:
+            # Set the 7th and 8th nodes of self to 3rd and 2nd nodes of other, respectively
+            TokenSpot.pair_adjacent(self._spots[6], other._spots[3])
+            TokenSpot.pair_adjacent(self._spots[7], other._spots[2])
+        else:
+            # Improper imput, don't mess anything up by doing anything
+            print("In MapSquare.set_adjacent(): encountered invalid side argument: " 
+                + str(side), stderr)
+
+        
 
     
 
--- game/map_square_test.py
+++ game/map_square_test.py
@@ -1,4 +1,5 @@
 from .map_square import MapSquare
+from .map_square import Side
 from .map_card import MapCard
 from .token_spot import TokenSpot
 from .player import Player
@@ -39,4 +40,126 @@ def test_place_card():
     tsp0.arrive_via_adjacent(token)
 
     assert tsp1.get_occupant() == token
+
+def test_bind_top():
+    
+    tsp00 = TokenSpot()
+    tsp01 = TokenSpot()
+    spots0 = [tsp00,tsp01]
+    for _ in range(6):
+        spots0.append(TokenSpot())
+
+    square0 = MapSquare(spots0)
+    
+    tsp15 = TokenSpot()
+    tsp14 = TokenSpot()
+    spots1 = []
+    for _ in range(4):
+        spots1.append(TokenSpot())
+    spots1.append(tsp14)
+    spots1.append(tsp15)
+    for _ in range(2):
+        spots1.append(TokenSpot())
+
+    square1 = MapSquare(spots1)
+
+    square0.set_adjacent(square1, Side.TOP)
+
+    assert tsp00._next_card == tsp15
+    assert tsp01._next_card == tsp14
+    assert tsp14._next_card == tsp01
+    assert tsp15._next_card == tsp00
+
+def test_bind_right():
+    
+    tsp02 = TokenSpot()
+    tsp03 = TokenSpot()
+    spots0 = []
+    for _ in range(2):
+        spots0.append(TokenSpot())
+    spots0.append(tsp02)
+    spots0.append(tsp03)
+    for _ in range(4):
+        spots0.append(TokenSpot())
+
+    square0 = MapSquare(spots0)
+    
+    tsp16 = TokenSpot()
+    tsp17 = TokenSpot()
+    spots1 = []
+    for _ in range(6):
+        spots1.append(TokenSpot())
+    spots1.append(tsp16)
+    spots1.append(tsp17)
+
+    square1 = MapSquare(spots1)
+
+    square0.set_adjacent(square1, Side.RIGHT)
+
+    assert tsp02._next_card == tsp17
+    assert tsp03._next_card == tsp16
+    assert tsp16._next_card == tsp03
+    assert tsp17._next_card == tsp02
+
+def test_bind_bottom():
+    
+    tsp04 = TokenSpot()
+    tsp05 = TokenSpot()
+    spots0 = []
+    for _ in range(4):
+        spots0.append(TokenSpot())
+    spots0.append(tsp04)
+    spots0.append(tsp05)
+    for _ in range(2):
+        spots0.append(TokenSpot())
+
+    square0 = MapSquare(spots0)
+    
+    tsp10 = TokenSpot()
+    tsp11 = TokenSpot()
+    spots1 = []
+    spots1.append(tsp10)
+    spots1.append(tsp11)
+    for _ in range(6):
+        spots1.append(TokenSpot())
+
+    square1 = MapSquare(spots1)
+
+    square0.set_adjacent(square1, Side.BOTTOM)
+
+    assert tsp04._next_card == tsp11
+    assert tsp05._next_card == tsp10
+    assert tsp10._next_card == tsp05
+    assert tsp11._next_card == tsp04
+
+def test_bind_left():
+    
+    tsp06 = TokenSpot()
+    tsp07 = TokenSpot()
+    spots0 = []
+    for _ in range(6):
+        spots0.append(TokenSpot())
+    spots0.append(tsp06)
+    spots0.append(tsp07)
+
+    square0 = MapSquare(spots0)
+    
+    tsp12 = TokenSpot()
+    tsp13 = TokenSpot()
+    spots1 = []
+    for _ in range(2):
+        spots1.append(TokenSpot())
+    spots1.append(tsp12)
+    spots1.append(tsp13)
+    for _ in range(4):
+        spots1.append(TokenSpot())
+
+    square1 = MapSquare(spots1)
+
+    square0.set_adjacent(square1, Side.LEFT)
+
+    assert tsp06._next_card == tsp13
+    assert tsp07._next_card == tsp12
+    assert tsp12._next_card == tsp07
+    assert tsp13._next_card == tsp06
 
