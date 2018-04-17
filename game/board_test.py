52e3a35880eeeea3fade2fcc3f770d8866c90b6c
William <williamstogin2018@u.northwestern.edu>
2018-04-17 10:36:47 -0500

    Added board initialization and tests for board structure

--- game/board_test.py
+++ game/board_test.py
@@ -1,24 +1,24 @@
-from .board import Board
-from .board import BOARD_HEIGHT, BOARD_WIDTH
+from .Board import Board
+from .Board import BOARD_HEIGHT, BOARD_WIDTH
 
 def test_top_border_terminal():
     board = Board()
 
     for square in board._squares[0]:
         assert square._spots[0]._is_terminal_spot and square._spots[1]._is_terminal_spot
-        
+
 def test_bottom_border_terminal():
     board = Board()
 
     for square in board._squares[BOARD_HEIGHT-1]:
         assert square._spots[4]._is_terminal_spot and square._spots[5]._is_terminal_spot
-        
+
 def test_left_border_terminal():
     board = Board()
 
     for row in board._squares:
         assert row[0]._spots[6]._is_terminal_spot and row[0]._spots[7]._is_terminal_spot
-        
+
 def test_right_border_terminal():
     board = Board()
 
@@ -43,16 +43,12 @@ def test_vertical_connections():
 
     prev_row = []
     for row in board._squares:
-        print(row)
-        for i in range(BOARD_WIDTH-1):
+        for i in range(BOARD_WIDTH):
             square = row[i]
-            for spot in square._spots:
-                print(spot)
-
             if len(prev_row) == BOARD_WIDTH:
                 prev = prev_row[i]
-                assert square._spots[0]._next_card == prev._spots[5]
-                assert square._spots[1]._next_card == prev._spots[4]
                 assert prev._spots[4]._next_card == square._spots[1]
                 assert prev._spots[5]._next_card == square._spots[0]
-            prev_row = row
+                assert square._spots[0]._next_card == prev._spots[5]
+                assert square._spots[1]._next_card == prev._spots[4]
+        prev_row = row
