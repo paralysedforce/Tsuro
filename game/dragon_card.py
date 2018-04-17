d9d768eb9e12308e28c5a038e02ee2c67d9e8036
William Stogin <William@William-PC.(none)>
2018-04-16 19:48:46 -0500

    Added method for getting holder in DragonCard and some tests

--- game/dragon_card.py
+++ game/dragon_card.py
@@ -8,11 +8,27 @@ class DragonCard:
     def __init__(self):
        self._holding_player = None
 
+    """ Indicates whether this card is held 
+
+        returns -- True if held, False otherwise.
+    """
     def is_held(self):
         return self._holding_player is not None
 
+    """ Sets the current holder of this card
+
+        Args:
+            player -- instance of Player that is taking possession of the card.
+    """
     def set_holder(self, player):
         self._holding_player = player
+
+    """ Gets the current holder of this card
+
+        returns -- Player instance if this card is held, None otherwise
+    """
+    def get_holder(self):
+        return self._holding_player
 
     def __str__(self):
         return "Dragon card held by " + str(self._holding_player)
--- game/dragon_card_test.py
+++ game/dragon_card_test.py
@@ -1,4 +1,24 @@
 from .dragon_card import DragonCard
+from .deck import Deck
+from .player import Player
 
 def test_not_held_initial():
     assert not DragonCard().is_held()
+
+def test_set_holder_held():
+    m_card = DragonCard()
+    player = Player(Deck(), m_card)
+
+    m_card.set_holder(player)
+    assert m_card.is_held()
+
+    
+def test_get_holder_held():
+    m_card = DragonCard()
+    player = Player(Deck(), m_card)
+
+    m_card.set_holder(player)
+    assert m_card.get_holder == player
+    
+def test_get_holder_not_held():
+    assert DragonCard().get_holder() is None
