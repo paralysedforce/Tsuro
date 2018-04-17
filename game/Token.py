e28c7601996d509c3fe9a6e8c4b293f09e06c064
William Stogin <William@William-PC.(none)>
2018-04-16 21:42:45 -0500

    Added documentation where it was lacking in Token

--- game/Token.py
+++ game/Token.py
@@ -1,8 +1,17 @@
 # { position:TokenSpot, associatedPlayer: Player }
 
 class Token:
+
+    """ Initializes a token for the given player.
+
+        Args:
+            player -- that the Token is associated with
+    """
     def __init__(self, player):
         self._player = player
 
+    """ Eliminates the token from the board,
+        which also triggers elimination of the associated player.
+    """
     def eliminate(self):
         self._player.eliminate()
