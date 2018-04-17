434e90a89c762e13f33a5df06acf8c35b2b7ff31
William Stogin <William@William-PC.(none)>
2018-04-16 21:36:47 -0500

    Added token implemntation and test for elimination

--- game/Token.py
+++ game/Token.py
@@ -1,6 +1,8 @@
 # { position:TokenSpot, associatedPlayer: Player }
 
 class Token:
-    def __init__(self):
-        self.position = None
-        self.associatedPlayer = None
+    def __init__(self, player):
+        self._player = player
+
+    def eliminate(self):
+        self._player.eliminate()
---
+++ game/token_test.py
@@ -0,0 +1,14 @@
+from .token import Token
+from .player import Player
+from .dragon_card import DragonCard
+from .deck import Deck
+
+def test_eliminate_player():
+    deck = Deck()
+    dragon = DragonCard()
+
+    player = Player(deck, dragon)
+    token = Token(player)
+
+    token.eliminate()
+    assert not player.is_active()
