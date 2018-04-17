43e1426d916fb95af3138a46eaa22910b66a4dbe
William Stogin <William@William-PC.(none)>
2018-04-17 08:29:50 -0500

    Partial implementation of board

--- game/Board.py
+++ game/Board.py
@@ -1,8 +1,49 @@
+from .map_square import MapSquare
+from .map_square import Side
+
 # { tokenSpots:TokenSpot[8][8], placementSquares: Dim2(4,4) }
+
+BOARD_HEIGHT = 6
+BOARD_WIDTH = 6
 
 class Board:
 
     def __init__(self):
-        self.tokenSpots = None
-        self.placementSquares = None
+
+        # init squares
+        self._squares = []
+        for _ in range(BOARD_HEIGHT):
+            row = []
+            for _ in range(BOARD_WIDTH):
+                square = MapSquare()
+                row.append(square)
+            self._squares.append(row)
+
+        # terminate boundary squares
+        for square in self._squares[0]:
+            square.set_terminal(Side.TOP)
+        for square in self._squares[BOARD_HEIGHT-1]:
+            square.set_terminal(Side.BOTTOM)
+        for squares in self._squares:
+            squares[0].set_terminal(Side.LEFT)
+            squares[BOARD_WIDTH-1].set_terminal(Side.RIGHT)
+        
+        # Bind adjacent squares            
+        prev_row = []
+        for row in self._squares:
+            prev_square = None
+            for i in range(BOARD_WIDTH):
+                square = row[i]
+
+                # Bind left
+                if prev_square is not None:
+                    square.set_adjacent(prev_square, Side.LEFT)
+                prev_square = square
+
+                # Bind above
+                if len(prev_row) > 0: # This was necessary because
+                    # subscripting prev_row was throwing errors
+                    square.set_adjacent(prev_row[i], Side.TOP)
+
+            prev_row = row
 
---
+++ game/board_test.py
@@ -0,0 +1,58 @@
+from .board import Board
+from .board import BOARD_HEIGHT, BOARD_WIDTH
+
+def test_top_border_terminal():
+    board = Board()
+
+    for square in board._squares[0]:
+        assert square._spots[0]._is_terminal_spot and square._spots[1]._is_terminal_spot
+        
+def test_bottom_border_terminal():
+    board = Board()
+
+    for square in board._squares[BOARD_HEIGHT-1]:
+        assert square._spots[4]._is_terminal_spot and square._spots[5]._is_terminal_spot
+        
+def test_left_border_terminal():
+    board = Board()
+
+    for row in board._squares:
+        assert row[0]._spots[6]._is_terminal_spot and row[0]._spots[7]._is_terminal_spot
+        
+def test_right_border_terminal():
+    board = Board()
+
+    for row in board._squares:
+        assert row[BOARD_WIDTH-1]._spots[2]._is_terminal_spot and row[BOARD_WIDTH-1]._spots[3]._is_terminal_spot
+
+def test_horizontal_connections():
+    board = Board()
+
+    for row in board._squares:
+        prev = None
+        for square in row:
+            if prev is not None:
+                assert square._spots[6]._next_card == prev._spots[3]
+                assert square._spots[7]._next_card == prev._spots[2]
+                assert prev._spots[2]._next_card == square._spots[7]
+                assert prev._spots[3]._next_card == square._spots[6]
+            prev = square
+
+def test_vertical_connections():
+    board = Board()
+
+    prev_row = []
+    for row in board._squares:
+        print(row)
+        for i in range(BOARD_WIDTH-1):
+            square = row[i]
+            for spot in square._spots:
+                print(spot)
+
+            if len(prev_row) == BOARD_WIDTH:
+                prev = prev_row[i]
+                assert square._spots[0]._next_card == prev._spots[5]
+                assert square._spots[1]._next_card == prev._spots[4]
+                assert prev._spots[4]._next_card == square._spots[1]
+                assert prev._spots[5]._next_card == square._spots[0]
+            prev_row = row
