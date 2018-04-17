534f49ae9eab209b0c253d9cd2bbe5725d6aef4f
William Stogin <William@William-PC.(none)>
2018-04-16 22:20:46 -0500

    Added TokenSpot implementation and tests

--- game/token_spot.py
+++ game/token_spot.py
@@ -1,7 +1,99 @@
+
 # { next: TokenSpot, previous: TokenSpot, occupant: Token }
 
 class TokenSpot:
-    def __init__(self):
-        self.next = None
-        self.previous = None
-        self.occupant = None
+
+    """ Initializes a TokenSpot.
+
+        Args:
+            is_terminal_spot=False -- whether this TokenSpot is terminal (edge of the board)
+            next_card=None -- The TokenSpot adjacent to this one.
+            next_spot=None -- The TokenSpot connected to this one via a path.
+    """
+    def __init__(self, is_terminal_spot=False, next_card=None, next_spot=None):
+        self._next_spot = next_spot
+        self._next_card = next_card
+        self._is_terminal_spot = is_terminal_spot
+        self._occupant = None
+
+    """ Eliminates any tokens as necessary
+
+        Args:
+            token -- that is arriving to trigger this check
+
+        return -- True if elimination occurred, False otherwis.
+    """
+    def _did_eliminate(self, token):
+        # Eliminate player if terminal
+        if self._is_terminal_spot:
+            token._player.eliminate()
+            return True
+
+        # If another occupant, eliminate both
+        elif self._occupant is not None:
+            token._player.eliminate()
+            self._occupant._player.eliminate()
+            self._occupant = None
+            return True
+        
+        return False
+
+
+    """ Called to notify of a token's arrival via a path on the same card
+
+        Args:
+            token -- that is arriving to this TokenSpot
+    """
+    def arrive_via_adjacent(self, token):
+        # Check if need to eliminate
+        if self._did_eliminate(token):
+            # Token eliminated, nothing else to do.
+            pass
+
+        # Pass along if possible
+        elif self._next_spot is not None:
+            self._next_spot.arrive_via_path(token)
+        else:
+            self._occupant = token
+
+    """ Called to notify of a token's arrival via adjacence from a different card
+
+        Args:
+            token -- that is arriving to this TokenSpot
+    """
+    def arrive_via_path(self,token):
+        # Check if need to eliminate
+        if self._did_eliminate(token):
+            # Token eliminated, nothing else to do.
+            pass
+
+        # Pass along if possible
+        elif self._next_card is not None:
+            self._next_card.arrive_via_adjacent(token)
+
+        # Make token occupant
+        else:
+            self._occupant = token
+
+    """ Pairs this TokenSpot to another via a path
+
+        Args:
+            other -- a TokenSpot connected via a path
+    """
+    def pair_via_path(self, other):
+        self._next_spot = other
+    
+    """ Pairs this TokenSpot to another via adjacency from a different card 
+
+        Args:
+            other -- a TokenSpot representing the same space on the board
+    """
+    def pair_via_adjacency(self, other):
+        self._next_card = other
+
+    """ Retrieves the occupying token from this TokenSpot
+
+        returns -- Token occupant of this TokenSpot
+    """
+    def get_occupant(self):
+        return self._occupant
---
+++ game/token_spot_test.py
@@ -0,0 +1,96 @@
+from .token_spot import TokenSpot
+from .player import Player
+from .deck import Deck
+from .dragon_card import DragonCard
+from .token import Token
+
+def create_token():
+    deck = Deck()
+    dragon = DragonCard()
+
+    player = Player(deck, dragon)
+    token = Token(player)
+    return token
+
+def test_token_occupant_arrival_adjacent():
+    token = create_token()
+
+    token_spot = TokenSpot()
+    token_spot.arrive_via_adjacent(token)
+    assert token == token_spot.get_occupant()
+
+
+def test_token_occupant_arrival_path():
+    token = create_token()
+
+    token_spot = TokenSpot()
+    token_spot.arrive_via_path(token)
+    assert token == token_spot.get_occupant()
+
+def test_token_passed_adjacent():
+    token = create_token()
+
+    token_spot1 = TokenSpot()
+    token_spot2 = TokenSpot()
+
+    token_spot1.pair_via_adjacency(token_spot2)
+    token_spot2.pair_via_adjacency(token_spot1)
+
+    token_spot1.arrive_via_path(token)
+
+    assert token_spot1.get_occupant() is None
+    assert token_spot2.get_occupant() == token
+
+def test_token_passed_path():
+    token = create_token()
+
+    token_spot1 = TokenSpot()
+    token_spot2 = TokenSpot()
+
+    token_spot1.pair_via_path(token_spot2)
+    token_spot2.pair_via_path(token_spot1)
+
+    token_spot1.arrive_via_adjacent(token)
+
+    assert token_spot1.get_occupant() is None
+    assert token_spot2.get_occupant() == token
+
+def test_terminal_elimination():
+    deck = Deck()
+    dragon = DragonCard()
+
+    player = Player(deck, dragon)
+    token = Token(player)
+
+    token_spot = TokenSpot(True)
+
+    assert token_spot._did_eliminate(token)
+
+def test_occupant_elimination():
+    deck = Deck()
+    dragon = DragonCard()
+
+    player = Player(deck, dragon)
+    player2 = Player(deck, dragon)
+    token = Token(player)
+    token2 = Token(player2)
+
+    token_spot = TokenSpot()
+    token_spot.arrive_via_adjacent(token)
+
+    assert token_spot._did_eliminate(token2)
+
+def test_both_elimination():
+    deck = Deck()
+    dragon = DragonCard()
+
+    player = Player(deck, dragon)
+    player2 = Player(deck, dragon)
+    token = Token(player)
+    token2 = Token(player2)
+
+    token_spot = TokenSpot()
+    token_spot.arrive_via_adjacent(token)
+    token_spot.arrive_via_path(token2)
+    
+    assert (not player.is_active()) and (not player2.is_active())
