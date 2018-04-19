affb3643027bed7b456e3436eb41bc88f0814ac3
William <williamstogin2018@u.northwestern.edu>
2018-04-18 20:38:44 -0500

    Updated documentation for player_token

--- tsuro/player_token.py
+++ tsuro/player_token.py
@@ -1,12 +1,20 @@
 # { position:TokenSpot, associatedPlayer: Player }
 
+
 class Token:
+    """Marks a player's location on the board.
+
+        Attributes:
+            _player (Player): who owns this token.
+            _location (TokenSpot): where this token currently is.
+    """
 
     """ Initializes a token for the given player.
 
         Args:
-            player -- that the Token is associated with
+            player (Player): who owns this token
     """
+
     def __init__(self, player):
         self._player = player
         player.set_token(self)
@@ -14,12 +22,11 @@ class Token:
 
     def set_location(self, spot):
         self._location = spot
-    
+
     def get_location(self):
         return self._location
 
-    """ Eliminates the token from the board,
-        which also triggers elimination of the associated player.
-    """
     def eliminate(self):
+        """Eliminates the token from the board,
+            triggering elimination of the associated player."""
         self._player.eliminate()
