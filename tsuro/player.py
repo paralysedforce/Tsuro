4609cd67154c85163f56c4c901b0f57a15c150dc
Eric <ericchang00@gmail.com>
2018-05-04 02:47:21 -0500

    use strategy pattern to encapsulate MoveStrategy

--- tsuro/player.py
+++ tsuro/player.py
@@ -1,3 +1,4 @@
+import random
 from abc import ABC, abstractmethod
 from enum import Enum
 from typing import List, Optional  # noqa: F401
@@ -35,23 +36,53 @@ class Player(State):
     has_moved: bool               = attrib(default=False)
 
 
-class MoveStrategy(ABC):
+# TODO: Our current player is actually a PlayerState.
+# class Player:
+#     def __init__(self):
+#         self.move_strategy = self.move_strategy_factory()
+
+#     def play_turn(board):
+#         return self.strategy.choose_move(board, self.tiles)
+
+#     def move_strategy_factory(self):
+#         return None
+
+
+class MoveStrategyInterface(ABC):
     """Encapsulation of a player's automatic move strategy."""
     @abstractmethod
-    def choose_move(board: Board, tiles: List[PathTile]) -> TilePlacement:
+    def choose_move(self, board: Board, tiles: List[PathTile]) -> TilePlacement:
         pass
 
 
-class RandomStrategy(MoveStrategy):
-    def choose_move(board: Board, tiles: List[PathTile]) -> TilePlacement:
+def validate_move_ability(func):
+    def decorated(self, board, tiles):
+        # Maybe this check should be in the Player class.
+        if not board.open_squares:
+            raise ValueError('Cannot choose move: Board is full.')
+        if not tiles:
+            raise ValueError('Cannot choose move: No tiles available.')
+        return func(self, board, tiles)
+    return decorated
+
+
+class RandomStrategy(MoveStrategyInterface):
+    @validate_move_ability
+    def choose_move(self, board: Board, tiles: List[PathTile]) -> TilePlacement:
+        return TilePlacement(
+            tile=random.choice(tiles),
+            coordinate=random.choice(board.open_squares),
+            rotation=random.choice(range(4)),
+        )
+
+
+class LeastSymmetricStrategy(MoveStrategyInterface):
+    @validate_move_ability
+    def choose_move(self, board: Board, tiles: List[PathTile]) -> TilePlacement:
         pass
 
 
-class LeastSymmetricStrategy(MoveStrategy):
-    def choose_move(board: Board, tiles: List[PathTile]) -> TilePlacement:
-        pass
-
-
-class MostSymmetricStrategy(MoveStrategy):
-    def choose_move(board: Board, tiles: List[PathTile]) -> TilePlacement:
+class MostSymmetricStrategy(MoveStrategyInterface):
+    @validate_move_ability
+    def choose_move(self, board: Board, tiles: List[PathTile]) -> TilePlacement:
         pass
